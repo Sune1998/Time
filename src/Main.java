b0fd6827487b233294329e2c47317a3332db14e7
sune <sune.mozart@gmail.com>
2019-11-26 14:50:49 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main extends Time{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Time.java
@@ -0,0 +1,30 @@
+public class Time {
+ public  long hour;
+ public  long min;
+ public  long sec;
+
+ public Time() {
+  long totalsec = System.currentTimeMillis() / 1000;
+  long totalmin = totalsec / 60;
+  long totalhour = totalmin / 60;
+  this.hour = totalhour %24;
+  this.min = totalmin % 60;
+  this.sec = totalsec & 60
+ }
+ public Time(long hour, long min, long sec) {
+  this.hour = hour;
+  this.min = min;
+  this.sec = sec;
+
+ }
+
+ public Time(long elapseTime) {
+  long totalsec = elapseTime / 1000;
+  long totalmin = totalsec / 60;
+  long totalhour = totalmin / 60;
+  this.hour = totalhour % 24;
+  this.min = totalmin % 60;
+  this.sec = totalsec % 60;
+ }
+
+}
